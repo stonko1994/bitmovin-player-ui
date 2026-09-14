25a216f625126c548d32fede32e824cd2b532577
David Steinacher <david.steinacher@bitmovin.com>
2026-09-14 14:22:45 -0600

    fix format based on new linter

--- src/ts/components/buttons/CustomCloseButton.ts
+++ src/ts/components/buttons/CustomCloseButton.ts
@@ -5,44 +5,47 @@ import { UIInstanceManager } from '../../UIManager';
 declare const window: any;
 
 interface CallbackInterface {
-    (data?: string): void;
+  (data?: string): void;
 }
 
 interface CustomMessageHandlerApi {
-    sendSynchronous(name: string, data?: string): string | null;
-    sendAsynchronous(name: string, data?: string): void;
-    on(methodName: string, callback: CallbackInterface): void;
+  sendSynchronous(name: string, data?: string): string | null;
+  sendAsynchronous(name: string, data?: string): void;
+  on(methodName: string, callback: CallbackInterface): void;
 }
 
 export class CustomCloseButton extends ToggleButton<ToggleButtonConfig> {
-    constructor(config: ToggleButtonConfig = {}) {
-        super(config);
+  constructor(config: ToggleButtonConfig = {}) {
+    super(config);
 
-        const defaultConfig: ToggleButtonConfig = {
-            cssClass: 'ui-custom-close-toggle-button',
-            text: 'close',
-        };
+    const defaultConfig: ToggleButtonConfig = {
+      cssClass: 'ui-custom-close-toggle-button',
+      text: 'close',
+    };
 
-        this.config = this.mergeConfig(config, defaultConfig, this.config);
-    }
+    this.config = this.mergeConfig(config, defaultConfig, this.config);
+  }
 
-    configure(player: PlayerAPI, uimanager: UIInstanceManager): void {
-        super.configure(player, uimanager);
+  configure(player: PlayerAPI, uimanager: UIInstanceManager): void {
+    super.configure(player, uimanager);
 
-        if (window.bitmovin.customMessageHandler) {
-            window.bitmovin.customMessageHandler.on('toggleCloseButton', (data?: string) => {
-                if (this.isEnabled()) {
-                    this.disable();
-                } else {
-                    this.enable();
-                }
-            });
-
-            this.onClick.subscribe(() => {
-                let result = window.bitmovin.customMessageHandler.sendSynchronous('closePlayer');
-                console.log('Return value from native:', result);
-                window.bitmovin.customMessageHandler.sendAsynchronous('closePlayerAsync');
-            });
+    if (window.bitmovin.customMessageHandler) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      window.bitmovin.customMessageHandler.on('toggleCloseButton', (data?: string) => {
+        if (this.isEnabled()) {
+          this.disable();
+        } else {
+          this.enable();
         }
+      });
+
+      this.onClick.subscribe(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const result = window.bitmovin.customMessageHandler.sendSynchronous('closePlayer');
+        console.log('Return value from native:', result);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        window.bitmovin.customMessageHandler.sendAsynchronous('closePlayerAsync');
+      });
     }
+  }
 }
